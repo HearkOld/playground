19ebf79a1e4101945bee59fd8a9ee1108c4b77e8
Heark <antwauntune25@wcpsstudent.org>
2016-05-13 09:28:58 -0400

    Update notif-favico.js

--- notif-favico.js
+++ notif-favico.js
@@ -1,101 +1,67 @@
-if (!Object.prototype.watch) {
-    Object.defineProperty(Object.prototype, "watch", {
-          enumerable: false
-        , configurable: true
-        , writable: false
-        , value: function (prop, handler) {
-            var
-              oldval = this[prop]
-            , newval = oldval
-            , getter = function () {
-                return newval;
-            }
-            , setter = function (val) {
-                oldval = newval;
-                return newval = handler.call(this, prop, oldval, val);
-            }
-            ;
+/*
+Notifications with Favico.js - Heark
+*/
 
-            if (delete this[prop]) { // can't watch constants
-                Object.defineProperty(this, prop, {
-                      get: getter
-                    , set: setter
-                    , enumerable: true
-                    , configurable: true
-                });
-            }
-        }
-    });
-}
-
-// object.unwatch
-if (!Object.prototype.unwatch) {
-    Object.defineProperty(Object.prototype, "unwatch", {
-          enumerable: false
-        , configurable: true
-        , writable: false
-        , value: function (prop) {
-            var val = this[prop];
-            delete this[prop]; // remove accessors
-            this[prop] = val;
-        }
-    });
-}
-        var o = { p: 0 };
-
-
-
-var notif = {
-      favicon: new Favico({
-    animation:'pop',
-    type: 'rectangle'
-}),
-    faviconNoAnim: new Favico({
-        animation: 'none',
-        type: 'rectangle'
-    }),
-    count: 0,
-    
-    update: function() {
-        notif.count++
-        o.p++
-        
-    },
-    
-    titleUpdate: function() {
-        o.watch('p', function (id, oldval, newval) {
-    notif.favicon.badge(notif.count)
-  return newval;
-});
-
-        if (notif.count == 0) {
-notif.favicon.badge("")
-} else {
-
-notif.faviconNoAnim.badge(notif.count)
-        }
-    },
-    
-    
-    clear: function() {
-        notif.count = 0;
-    },
-    
-    dec: function(){
-        notif.count--;
-    },
-    
-    // Adds a notification for each value in array
-    forEach: function(array) {
-        for(var i = 0; i < array.length; i++){
-            notif.update();
-        }
+Object.prototype.watch || Object.defineProperty(Object.prototype, "watch", {
+    enumerable: !1,
+    configurable: !0,
+    writable: !1,
+    value: function(t, n) {
+        var e = this[t],
+            o = e,
+            i = function() {
+                return o
+            },
+            c = function(i) {
+                return e = o, o = n.call(this, t, e, i)
+            };
+        delete this[t] && Object.defineProperty(this, t, {
+            get: i,
+            set: c,
+            enumerable: !0,
+            configurable: !0
+        })
     }
-    
-
-}
-
-setInterval(function () {
+}), Object.prototype.unwatch || Object.defineProperty(Object.prototype, "unwatch", {
+    enumerable: !1,
+    configurable: !0,
+    writable: !1,
+    value: function(t) {
+        var n = this[t];
+        delete this[t], this[t] = n
+    }
+});
+var o = {
+        p: 0
+    },
+    notif = {
+        favicon: new Favico({
+            animation: "pop",
+            type: "rectangle"
+        }),
+        faviconNoAnim: new Favico({
+            animation: "none",
+            type: "rectangle"
+        }),
+        count: 0,
+        update: function() {
+            notif.count++, o.p++
+        },
+        titleUpdate: function() {
+            o.watch("p", function(t, n, e) {
+                return notif.favicon.badge(notif.count), e
+            }), 0 == notif.count ? notif.favicon.badge("") : notif.faviconNoAnim.badge(notif.count)
+        },
+        clear: function() {
+            notif.count = 0
+        },
+        dec: function() {
+            notif.count--
+        },
+        forEach: function(t) {
+            for (var n = 0; n < t.length; n++) notif.update()
+        }
+    };
+setInterval(function() {
     notif.titleUpdate()
-}, 1000);
-    
+}, 1e3);
